8b90c1545c9f9a635116fdc3859b8b9877606969
Kuba <k.k.wasilewski@gmail.com>
2020-01-09 11:59:25 +0100

    initial working commit

---
+++ src/main/java/bananapp/Quickstart.java
@@ -0,0 +1,81 @@
+package bananapp;
+
+import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.gax.paging.Page;
+import com.google.api.services.iam.v1.Iam;
+import com.google.api.services.iam.v1.IamScopes;
+import com.google.api.services.iam.v1.model.ListRolesResponse;
+import com.google.api.services.iam.v1.model.Role;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+import com.google.common.collect.Lists;
+import org.apache.log4j.BasicConfigurator;
+
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Quickstart {
+
+    public static void GetData() throws Exception {
+        // You can specify a credential file by providing a path to GoogleCredentials.
+        // Otherwise credentials are read from the GOOGLE_APPLICATION_CREDENTIALS environment variable.
+        List results = new ArrayList();
+
+        GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream("/home/kuba/Desktop/bananas/settings/bananapp-264116-0483d7ffaad7.json"))
+                .createScoped(Lists.newArrayList("https://www.googleapis.com/auth/cloud-platform"));
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
+
+        System.out.println("Buckets:---------------------------------------------------------------------------");
+        Page<Bucket> buckets = storage.list();
+        for (Bucket bucket : buckets.iterateAll()) {
+            System.out.println(bucket.toString());
+        }
+        System.out.println("Credentials:--------------------------------------------------------------------");
+        System.out.println(credentials);
+
+        StorageOptions storageOptions = StorageOptions.newBuilder()
+                .setProjectId("bananapp-264116")
+                .setCredentials(GoogleCredentials.fromStream(new
+                        FileInputStream("/home/kuba/Desktop/bananas/settings/bananapp-264116-0483d7ffaad7.json"))).build();
+
+        //Storage storage = storageOptions.getService();
+        // Get credentials
+        GoogleCredential credential =
+                GoogleCredential.getApplicationDefault()
+                        .createScoped(Collections.singleton(IamScopes.CLOUD_PLATFORM));
+
+        // Create the Cloud IAM service object
+        Iam service =
+                new Iam.Builder(
+                        GoogleNetHttpTransport.newTrustedTransport(),
+                        JacksonFactory.getDefaultInstance(),
+                        credential)
+                        .setApplicationName("quickstart")
+                        .build();
+
+        // Call the Cloud IAM Roles API
+        ListRolesResponse response = service.roles().list().execute();
+        List<Role> roles = response.getRoles();
+
+        // Process the response
+        System.out.println("Roles:----------------------------------------------------------");
+        for (Role role : roles) {
+            System.out.println("Title: " + role.getTitle());
+            System.out.println("Name: " + role.getName());
+            System.out.println("Description: " + role.getDescription());
+            System.out.println();
+        }
+
+        //System.out.println("Models:---------------------------------------------------------------");
+        //ListModels.listModels();
+
+        System.out.println("Prediction:---------------------------------------------------------");
+        VisionClassificationPredict.main(new String[0]);
+    }
+}
---
+++ src/main/java/bananapp/VisionClassificationPredict.java
@@ -0,0 +1,55 @@
+package bananapp;
+
+import com.google.cloud.automl.v1.AnnotationPayload;
+import com.google.cloud.automl.v1.ExamplePayload;
+import com.google.cloud.automl.v1.Image;
+import com.google.cloud.automl.v1.ModelName;
+import com.google.cloud.automl.v1.PredictRequest;
+import com.google.cloud.automl.v1.PredictResponse;
+import com.google.cloud.automl.v1.PredictionServiceClient;
+import com.google.protobuf.ByteString;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+class VisionClassificationPredict {
+
+  public static void main(String[] args) throws IOException {
+    // TODO(developer): Replace these variables before running the sample.
+    String projectId = "bananapp-264116";
+    String modelId = "ICN1230199579853455360";
+    String filePath = "/home/kuba/Downloads/b.jpg";
+    predict(projectId, modelId, filePath);
+  }
+
+  static void predict(String projectId, String modelId, String filePath) throws IOException {
+    // Initialize client that will be used to send requests. This client only needs to be created
+    // once, and can be reused for multiple requests. After completing all of your requests, call
+    // the "close" method on the client to safely clean up any remaining background resources.
+    try (PredictionServiceClient client = PredictionServiceClient.create()) {
+      // Get the full path of the model.
+      ModelName name = ModelName.of(projectId, "us-central1", modelId);
+      ByteString content = ByteString.copyFrom(Files.readAllBytes(Paths.get(filePath)));
+      Image image = Image.newBuilder().setImageBytes(content).build();
+      ExamplePayload payload = ExamplePayload.newBuilder().setImage(image).build();
+      PredictRequest predictRequest =
+              PredictRequest.newBuilder()
+                      .setName(name.toString())
+                      .setPayload(payload)
+                      .putParams(
+                              "score_threshold", "0.8") // [0.0-1.0] Only produce results higher than this value
+                      .build();
+
+      PredictResponse response = client.predict(predictRequest);
+
+      for (AnnotationPayload annotationPayload : response.getPayloadList()) {
+        System.out.format("Predicted class name: %s\n", annotationPayload.getDisplayName());
+        System.out.format(
+                "Predicted class score: %.2f\n", annotationPayload.getClassification().getScore());
+      }
+    }
+  }
+}
